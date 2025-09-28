0643ac76648071d0817c28f1805c867ffed176d5
dioliver29 <diana.oliver29@gmail.com>
2025-09-28 16:10:17 +0500

    hw ts 15

---
+++ homework-solutions/ts/hw-15/task-2.ts
@@ -0,0 +1,24 @@
+/* Напишите дженерик-функцию getKeyByValue, которая принимает объект и значение, и возвращает ключ, соответствующий этому значению. 
+Если значение не найдено, функция должна возвращать undefined.
+Используйте keyof для типизации ключей объекта
+ */
+
+const example = {
+  id: 1,
+  name: 'Diana',
+  surname: 'Jnonson',
+};
+
+function getKeyByValue<T extends Record<string, any>, V>(obj: T, value: V): keyof T | undefined {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+console.log(getKeyByValue(example, 'Diana'));
+console.log(getKeyByValue(example, 1));
+console.log(getKeyByValue(example, 'Heyri'));
+
